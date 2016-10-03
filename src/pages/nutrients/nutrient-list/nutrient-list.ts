9a353995674c6757eec351462ca35b7668e1071d
razvantomegea <razvan.tomegea@gmail.com>
2016-10-03 18:37:41 +0300

    nutrients did not show in nutrient-list the ngSwitchCase needs a string value

--- src/pages/nutrients/nutrient-list/nutrient-list.ts
+++ src/pages/nutrients/nutrient-list/nutrient-list.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Alert, NavController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { FirebaseListObservable } from 'angularfire2';
 
 // Models
